4e1cd74953d6fa782060aa963f5cbe177e7d80e2
Nicholas Gersbach <54191233+Nick-Gersbach@users.noreply.github.com>
2022-10-19 15:09:36 -0700

    spaces added every 4 number for cc with js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+//Add Spaces every 4 Numbers for CC Number
+document
+  .getElementById("cc-number-input")
+  .addEventListener("input", function (e) {
+    const target = e.target,
+      position = target.selectionEnd,
+      length = target.value.length;
 
+    target.value = target.value
+      .replace(/[^\dA-Z]/g, "")
+      .replace(/(.{4})/g, "$1 ")
+      .trim();
+    target.selectionEnd = position +=
+      target.value.charAt(position - 1) === " " &&
+      target.value.charAt(length - 1) === " " &&
+      length !== target.value.length
+        ? 1
+        : 0;
+  });
